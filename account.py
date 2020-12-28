c731f6db4c9fb715d893f1624d0379dde91887db
ekonovalov <egor.konovalov@vmssoftware.com>
2020-12-28 16:04:39 +0300

    #2. Fixed accounts' information return

--- account.py
+++ account.py
@@ -14,27 +14,26 @@ class Account:
         self._balance = balance
 
     def __str__(self):
-        return 'Account [' + str(self._number) + '] - '
-    
+        return 'Account [' + str(self._number) + '] - ' + self._name
+
     @property
     def balance(self):
         """ The docstring for the balance property """
-        print('In Balance method')
         return self._balance
 
 class CurrentAccount(Account):
     def __init__(self, number, name, balance, overdraftLimit):
         super().__init__(number, name, balance)
         self.overdraftLimit = overdraftLimit
-    
+
     def __str__(self):
-        return super().__str__() + 'current account = ' + str(self._balance) + ', overdraft limit is: ' + str(self.overdraftLimit)
+        return super().__str__() + ', current account = ' + str(self._balance) + ', overdraft limit: ' + str(self.overdraftLimit)
 
     def withdraw(self, amount):
         if amount < self.overdraftLimit:
             self._balance -= amount
         else: print('Withdrawal would exceed your overdraft limit')
-    
+
     def deposit(self, amount):
         self._balance += amount
 
@@ -43,9 +42,9 @@ class DepositAccount(Account):
     def __init__(self, number, name, balance, interestRate):
         super().__init__(number, name, balance)
         self.interestRate = interestRate
-    
+
     def __str__(self):
-        return super().__str__() + 'de'
+        return super().__str__() + ', savings account = ' + str(self._balance) + ', interest rate: ' + str(self.interestRate)
 
 class InvestmentAccount(Account):
 
@@ -53,7 +52,10 @@ class InvestmentAccount(Account):
         super().__init__(number, name, balance)
         self.investmentType = investmentType
 
-acc1 = CurrentAccount('123', 'John', 10.05, 100.0) 
+    def __str__(self):
+        return super().__str__() + ', investment account = ' + str(self._balance)
+
+acc1 = CurrentAccount('123', 'John', 10.05, 100.0)
 acc2 = DepositAccount('345', 'John', 23.55, 0.5)
 acc3 = acc3 = InvestmentAccount('567', 'Phoebe', 12.45, 'high risk')
 
@@ -66,6 +68,6 @@ acc1.withdraw(12.33)
 print('balance:', acc1.balance)
 
 print('Number of Account instances created:', Account.instance_count)
-print('balance:', acc1.balance) 
+print('balance:', acc1.balance)
 acc1.withdraw(300.00)
 print('balance:', acc1.balance)
